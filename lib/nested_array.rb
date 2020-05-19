3be8396fcf0f3dbe74aa1aa6bcae20c5ea1c223f
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-19 03:05:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -27,8 +27,8 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
-  fruits = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
-  fruits[1][1]
+  matrix = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
